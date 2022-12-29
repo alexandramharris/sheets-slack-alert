88ec705d91ecff60912cd9786b07f6316d24d2d6
Alexandra Harris <103076913+alexandramharris@users.noreply.github.com>
2022-12-28 23:12:43 -0500

    incorporates date column
    script now accounts for multiple entries by the same company by checking if the date of the entry in B2:B is new

--- alert-script.js
+++ alert-script.js
@@ -2,37 +2,39 @@ function CheckNotices() {
   try {
     var companyNamesRange = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Notices").getRange("A2:A");
     var companyNames = companyNamesRange.getValues();
-
+    var companyDatesRange = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Notices").getRange("B2:B");
+    var companyDates = companyDatesRange.getValues();
     console.log("companyNames:", companyNames);
-
     var processedCompanyNames = getProcessedCompanyNames();
-
     console.log("processedCompanyNames:", processedCompanyNames);
-
-    for (var i = 0; i < companyNames.length; i++) {
-      var companyName = companyNames[i][0];
-      if (companyName && !processedCompanyNames.includes(companyName)) {
-        processedCompanyNames.push(companyName);
-        if (companyName.includes("Capital Region") || companyName.includes("Mid-Hudson Region")) {
-          var text = ":rotating_light: *New local layoff notice* :rotating_light: \nFrom " + companyName + "\n\nCheck out the full notice: https://dol.ny.gov/warn-notices \n<!here>";
-        } else {
-          var text = ":bell: *New layoff notice in New York* :bell: \nFrom " + companyName + "\n\nCheck out the full notice: https://dol.ny.gov/warn-notices";
-        }
-        // Send slack alert
-        sendSlackAlert(text);
-      }
+  var processedCompanyNames = [];
+  for (var i = 0; i < companyNames.length; i++) {
+    var companyName = companyNames[i][0];
+    var companyDate = companyDates[i][0];
+    processedCompanyNames.push({name: companyName, date: companyDate});
+  }
+  for (var i = 0; i < companyNames.length; i++) {
+    var companyName = companyNames[i][0];
+    var companyDate = companyDates[i][0];
+    var text;
+    if (companyName.includes("Capital Region") || companyName.includes("Mid-Hudson Region")) {
+      text = ":rotating_light: *New local layoff notice* :rotating_light: \nFrom " + companyName + "\n\nCheck out the full notice: https://dol.ny.gov/warn-notices \n<!here>";
+    } else {
+      text = ":bell: *New layoff notice in New York* :bell: \nFrom " + companyName + "\n\nCheck out the full notice: https://dol.ny.gov/warn-notices";
     }
-
+    if (!ScriptProperties.getProperty(companyName + companyDate)) {
+      ScriptProperties.setProperty(companyName + companyDate, true);
+      sendSlackAlert(text);
+    }
+  }
     updateProcessedCompanyNames(processedCompanyNames);
   } catch (error) {
     console.error(error);
   }
 }
-
 function updateProcessedCompanyNames(new_value) {
   ScriptProperties.setProperty("processedCompanyNames", JSON.stringify(new_value));
 }
-
 function getProcessedCompanyNames() {
   var processedCompanyNames = ScriptProperties.getProperty("processedCompanyNames");
   if (processedCompanyNames) {
@@ -41,14 +43,11 @@ function getProcessedCompanyNames() {
     return [];
   }
 }
-
 function sendSlackAlert(text) {
-  var url = "URL GOES HERE";
+  var url = "URL HERE";
   var payload = {
     text: text
   }
-
-
   var headers = {
     'Content-type': 'application/json'
   }
